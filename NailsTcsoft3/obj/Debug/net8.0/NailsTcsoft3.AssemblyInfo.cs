32f29fa3fd85be7c20d8a8c78b601d18a43b4e09
KhuatDuyTien2003 <kdt0974758285@gmail.com>
2025-05-12 15:41:56 +0700

    Resolve merge conflicts by keeping local changes (HEAD)

--- NailsTcsoft3/obj/Debug/net8.0/NailsTcsoft3.AssemblyInfo.cs
+++ NailsTcsoft3/obj/Debug/net8.0/NailsTcsoft3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NailsTcsoft3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e84dfcb61fc7d7ababeb8fdb101e6b9efe5fe35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7b37af8df7452d185e58c5a419e294d403e02a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NailsTcsoft3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NailsTcsoft3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
